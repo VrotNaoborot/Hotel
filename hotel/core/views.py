55b9a98902ffb202942a648ea37080e41f677616
Panu.Mac <nktleznin@gmail.com>
2024-11-25 13:53:17 +0300

    add booking.html

--- hotel/core/views.py
+++ hotel/core/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 
 
 def main(request):
     return render(request, 'index.html')
+
+
+def booking(request):
+    return render(request, 'booking.html')
