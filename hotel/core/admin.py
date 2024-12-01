71de4e20ae4214e745107e51c8bb212848e47f9c
Panu.Mac <nktleznin@gmail.com>
2024-12-01 17:12:17 +0300

    add models

--- hotel/core/admin.py
+++ hotel/core/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from django import forms
+from .models import CategoryRoom
 
-# Register your models here.
+# Список доступных удобств
+AVAILABLE_AMENITIES = [
+    "Wi-Fi",
+    "Кондиционер",
+    "Телевизор",
+    "Холодильник",
+    "Сейф",
+    "Душ",
+]
+
+
+class CategoryRoomAdminForm(forms.ModelForm):
+    # Перекрываем поле amenities для кастомного отображения
+    amenities = forms.MultipleChoiceField(
+        choices=[(amenity, amenity) for amenity in AVAILABLE_AMENITIES],
+        widget=forms.CheckboxSelectMultiple,
+        required=False,
+        label="Удобства",
+    )
+
+    class Meta:
+        model = CategoryRoom
+        fields = "__all__"
+
+    def clean_amenities(self):
+        # Преобразуем список выбранных значений в JSON-совместимый формат
+        return self.cleaned_data["amenities"]
+
+
+class CategoryRoomAdmin(admin.ModelAdmin):
+    form = CategoryRoomAdminForm
+
+
+admin.site.register(CategoryRoom, CategoryRoomAdmin)
--- hotel/core/models.py
+++ hotel/core/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
+
 # Create your models here.
+
+class CategoryRoom(models.Model):
+    name = models.CharField(max_length=150, verbose_name="Название")
+    capacity = models.IntegerField(verbose_name="Вместимость")
+    space = models.IntegerField(verbose_name="Площадь")
+    count_room = models.IntegerField(verbose_name="Кол-во комнат")
+    amenities = models.JSONField(verbose_name="Удобства", default=list)
+    img1 = models.ImageField(verbose_name="Изображение 1")
+    img2 = models.ImageField(verbose_name="Изображение 2")
+    img3 = models.ImageField(verbose_name="Изображение 3")
+    img4 = models.ImageField(verbose_name="Изображение 4")
+
+    def __str__(self):
+        return self.name
+
+
+class Room(models.Model):
+    number = models.IntegerField(primary_key=True, verbose_name="Номер комнаты")
+    category = models.ForeignKey(CategoryRoom, on_delete=models.CASCADE)
+    is_available = models.BooleanField(default=False, verbose_name="Доступна")
+
+
+class Booking(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, verbose_name="Имя")
+    surname = models.CharField(max_length=255, verbose_name="Фамилия")
+    middle_name = models.CharField(max_length=255, verbose_name="Отчество")
+    number = models.CharField(max_length=30, verbose_name="Номер телефона")
+    mail = models.CharField(max_length=100, verbose_name="Почта")
+    total_price = models.IntegerField(verbose_name="Итоговая цена")
